572d59174e6867800e174723cc31a6776a2dc383
Quinton Dean <qdean@implantbase.com>
2022-11-01 09:33:50 -0500

    got health check running and tested

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
+/*
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        // this requires changing the return type of `health_check`
+        assert!(response.status().is_success())
+    }
+}
+*/
