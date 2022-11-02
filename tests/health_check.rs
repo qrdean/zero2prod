7df44b6dd156eac99b80ccc308196690a14ea50b
Quinton Dean <qdean@implantbase.com>
2022-11-01 23:32:39 -0500

    add postgres to 200 test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,9 @@
 //! tests/health_check.rs
-use zero2prod::startup::run;
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+
 
 #[tokio::test]
 async fn health_check_works() {
@@ -21,6 +24,12 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_return_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -32,7 +41,15 @@ async fn subscribe_return_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
 
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
