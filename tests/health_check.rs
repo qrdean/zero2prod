a2e0c38581e416a446a10edb424fb547033839c2
Quinton Dean <qdean@implantbase.com>
2022-11-03 21:39:22 -0500

    refactor database impl

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,7 +5,6 @@ use once_cell::sync::Lazy;
 use zero2prod::startup::run;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::telemetry::{init_subscriber, get_subscriber};
-use secrecy::ExposeSecret;
 use uuid::Uuid;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -116,7 +115,7 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
 
@@ -126,7 +125,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to create database");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
