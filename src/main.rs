4e5e2048997648e1fe33dacc9b7b8d8c5122e424
Quinton Dean <qdean@implantbase.com>
2022-11-03 21:48:54 -0500

    change configuration for ssl

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::postgres::PgPoolOptions;
-use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
